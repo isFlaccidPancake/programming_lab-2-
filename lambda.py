df1af16b943de33a2e4da3a5ed69f587bb28f631
isFlaccidPancake <pierlu.petrianni@gmail.com>
2026-04-23 18:42:36 +0200

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -1,3 +1,3 @@
-x,y= int(input()), int(input())
+x,y= int(input()), int(input())  #page 85 Guttag
 function= lambda a , b: none if b==0 else a/b #a and b are local variables, order matters
 print(function(x,y))
